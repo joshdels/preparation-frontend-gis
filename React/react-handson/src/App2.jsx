305035ecb54377660bdc381351164520853d770e
joshdels <deleonjoshy2@gmail.com>
2025-10-17 21:16:16 +0800

    finish mini project faq shower

--- React/react-handson/src/App2.jsx
+++ React/react-handson/src/App2.jsx
@@ -2,19 +2,46 @@ import { useState } from 'react'
 
 
 function App2() {
-  // Temperature
+  // Temperature Converter
   const [temp, setTemp] = useState(0);
-  const [convert, setConvert] = useState("Celcius")
-  const [isBoiling, setIsBoiling] = useState(false);
+  const [isCelsius, setIsCelsius] = useState(true);
 
 
+
+  function handleTemp(e) {
+    setTemp(Number(e.target.value));
+  }
+
+  function convertTemp(value, toCelsius) {
+    return toCelsius ? (value - 32) * 5/9 : (value * 9/5) + 32;
+  }
+
+  function handleSubmitConvert() {
+    setTemp(convertTemp(temp, !isCelsius));
+    setIsCelsius(!isCelsius);
+  }
 
   return (
     <> 
       <h1>Temperature</h1>
       <label>Temp</label>
-      <input type="text" />
-      <input type="text" />
+      <input 
+        type="number"
+        value={temp}
+        onChange={handleTemp}
+      />
+      <button
+        onClick={handleSubmitConvert}
+      >
+        Convert to {isCelsius ? "°F" : "°C"}
+      </button>
+      <p>
+        {
+          ((isCelsius && temp >= 100) 
+          || (!isCelsius && temp >= 212))
+          && "100 C reached!"
+        }
+      </p>
     </>
   )
 }
