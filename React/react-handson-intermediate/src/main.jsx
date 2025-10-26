7922c59587959809564d1af42535530675e97d66
joshdels <deleonjoshy2@gmail.com>
2025-10-26 22:03:32 +0800

    fin global theme using context api

--- React/react-handson-intermediate/src/main.jsx
+++ React/react-handson-intermediate/src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import App2 from './App2.jsx'
 import App4 from './App4.jsx'
+import App5 from './App5.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
     {/* <App2 /> */}
-    <App4 />
+    {/* <App4 /> */}
+    <App5 />
   </StrictMode>,
 )
